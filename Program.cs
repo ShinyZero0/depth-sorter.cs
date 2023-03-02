2a3020d8b0994f775c7d4b00918461750d532d91
Zero <shinyzero0@gmail.com>
2023-03-02 17:17:22 +0300

    init. works

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.Collections.Generic;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var output = new SortedDictionary<int, List<string>>();
+        string s;
+        while(( s = Console.ReadLine()) != null)
+        {
+            string file = Console.ReadLine();
+            int depth;
+            depth = file.Count(c => c == '/');
+            if (!file.EndsWith('/'))
+            { 
+                depth++;
+            }
+            if (output.ContainsKey(depth) == false)
+            {
+                output[depth] = new List<string>();
+            }
+            output[depth].Add(file);
+        }
+        foreach (var lines in output)
+        {
+            foreach (var line in lines.Value)
+            {
+                Console.WriteLine(line);
+            }
+
+        }
+
+    }
+}
